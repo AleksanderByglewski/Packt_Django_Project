984affecb800afed42aac01c483192b0dbe511a7
Alex <alexbyglewski@icloud.com>
2021-06-06 21:05:37 +0200

    Finished project

--- btre/urls.py
+++ btre/urls.py
@@ -20,5 +20,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('',include('pages.urls'),name='index'),
     path('listings/',include('listings.urls'),name='listings'),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('accounts/',include('accounts.urls')),
+    path('contacts/',include('contacts.urls')),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
